d484d638447a3c3373a458aceb2737bfbbef65ea
shusaku009 <shusaku009@gmail.com>
2023-10-31 23:39:31 +0900

    add: Atomの作成

--- src/components/atoms/SecondaryButton.jsx
+++ src/components/atoms/SecondaryButton.jsx
@@ -1,19 +1,11 @@
 import styled from "styled-components"
+import { BaseButton } from "./BaseButton";
 
 export const SecondaryButton = (props) => {
   const { children } = props;
   return <SButton>{children}</SButton>;
 };
 
-const SButton = styled.button`
-  background-color: #11999ef;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  outline: none;
-  border-radius: 9999px;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
+const SButton = styled(BaseButton)`
+  background-color: #11999e;
 `;
