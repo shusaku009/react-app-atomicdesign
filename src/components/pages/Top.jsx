315500ce42b2774262e0289b027d98de13351164
shusaku009 <shusaku009@gmail.com>
2023-11-03 14:49:47 +0900

    add: 管理者、一般ユーザーボタンを作成

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,20 @@
+import React from "react";
 import styled from "styled-components"
+import { useHistory } from "react-router-dom";
+import { SecondaryButton } from "../atoms/button/SecondaryButton"
 
 export const Top = () => {
+  const history = useHistory();
+
+  const onClickAdmin = () => history.push("/users");
+  const onClickGeneral = () => history.push("/users");
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
