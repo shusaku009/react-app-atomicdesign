b80ba540b6d3dccbeca1aa7c4cac916b28b8aa93
shusaku009 <shusaku009@gmail.com>
2023-11-04 09:32:00 +0900

    add: Contextでのstate管理

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components"
-import { useLocation } from "react-router-dom";
 
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
@@ -20,15 +19,13 @@ const users = [...Array(10).keys()].map(((val) => {
 }));
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
       <SUserArea>
         {users.map((obj) => (
-          <UserCard key={obj.id} user={obj} isAdmin={isAdmin} />
+          <UserCard key={obj.id} user={obj} />
         ))}
       </SUserArea>
     </SContainer>
